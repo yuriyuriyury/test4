3466183098e37b5d4d54d5982205482d6d0164b0
“yuri”~ <rurasuton@gmail.com>
2024-08-25 14:29:02 +0900

    no message

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-# app.py
+import os
 import gradio as gr
 
+# Gradioアプリケーションの定義
 def greet(name):
     return "Hello " + name + "!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch(share=True)
+
+# ポートを環境変数から取得
+port = int(os.environ.get('PORT', 8000))
+
+# Gradioアプリケーションの起動
+iface.launch(server_name="0.0.0.0", server_port=port)
